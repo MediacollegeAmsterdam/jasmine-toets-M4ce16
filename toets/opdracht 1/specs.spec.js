5c255e9bab4d13667a2f46bea5ad4214296639fa
Mika <54840853+M4ce16@users.noreply.github.com>
2020-10-20 10:07:07 +0200

    opdracht 1

--- toets/opdracht 1/specs.spec.js
+++ toets/opdracht 1/specs.spec.js
@@ -1,3 +1,4 @@
+//verbeterd
 describe('Testje 1', function() {
     it('should have the correct title', function () {
         expect(document.title).toBe('Voorbeeld toets');
@@ -7,8 +8,8 @@ describe('Testje 1', function() {
 
 describe('Testje 2', function () {
     it('should test', function () {
-        expect(unitTest.item).toBe('voorbeeld');
-        it('should test', function () {
+        expect(unitTest.item).toBe('item');
+        ('should test', function () {
             expect(unitTest.nummer).toEqual(3);
         });
     });
@@ -17,10 +18,38 @@ describe('Testje 2', function () {
 describe('Testje 3', function () {
     it('should test', function () {
         expect(unitTest.beschrijving).toBe('Beschrijving');
-        describe('Testje 4', function () {
+         ('Testje 4', function () {
             it('should test', function () {
                 expect(unitTest.boodschap).toEqual('Hallo');
             });
         });
     });
 });
+
+//orgineel
+// describe('Testje 1', function() {
+//     it('should have the correct title', function () {
+//         expect(document.title).toBe('Voorbeeld toets');
+//         expect(document.charset).toBe('UTF-8');
+//     });
+// });
+//
+// describe('Testje 2', function () {
+//     it('should test', function () {
+//         expect(unitTest.item).toBe('voorbeeld');
+//         it('should test', function () {
+//             expect(unitTest.nummer).toEqual(3);
+//         });
+//     });
+// });
+//
+// describe('Testje 3', function () {
+//     it('should test', function () {
+//         expect(unitTest.beschrijving).toBe('Beschrijving');
+//         describe('Testje 4', function () {
+//             it('should test', function () {
+//                 expect(unitTest.boodschap).toEqual('Hallo');
+//             });
+//         });
+//     });
+// });
